0e9c7380a5e2606b56988b86d3d1e9a443cbcfbc
Scott R. Sonnenwald <ssonnenwald@gmail.com>
2025-04-14 00:08:24 -0400

    Initial Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'content-projection',
+  },
+  {
+    title: 'Content Projection',
+    path: 'content-projection',
+    loadComponent: () =>
+      import('./components/content-projection/content-projection.component'),
+  },
+  { path: '**', redirectTo: '' }, // Redirect unmatched routes
+];
---
+++ src/app/components/content-projection/content-projection.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-projection',
+  imports: [],
+  templateUrl: './content-projection.component.html',
+  styleUrl: './content-projection.component.scss',
+})
+export default class ContentProjectionComponent {}
